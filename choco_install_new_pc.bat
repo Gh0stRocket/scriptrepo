bec9728f59cb98bbce9f1157e7e4d644f10ef981
Falk Haehner <falkhaehner@aol.com>
2020-12-17 15:29:38 +0100

    Update choco_install_new_pc.bat

--- choco_install_new_pc.bat
+++ choco_install_new_pc.bat
@@ -1,1 +1,17 @@
-choco install -y steam origin brave epicgameslauncher teamviewer sublimetext3 geforce-experience geforce-game-ready-driver cpu-z msiafterburner gpu-z discord uplay bethesdanet 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+:: Put double colon before each software u do not want to install
+choco install -y ^
+steam ^
+origin ^
+brave ^
+epicgameslauncher ^
+teamviewer ^
+sublimetext3 ^
+geforce-experience ^
+geforce-game-ready-driver ^
+cpu-z ^
+msiafterburner ^
+gpu-z ^
+discord ^
+uplay ^
+bethesdanet
